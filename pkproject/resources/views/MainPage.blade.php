37797cf0e5135e0b0a5bfd038a644f773cf2a5fd
IvanAmorim <93208193+IvanAmorim@users.noreply.github.com>
2022-12-21 16:14:53 +0000

    mainpage

--- pkproject/resources/views/MainPage.blade.php
+++ pkproject/resources/views/MainPage.blade.php
@@ -71,9 +71,59 @@
                 <span class="casa">Serviços empresariais</span>
             </a>
         </div>
-        
     </div>
+    <div class="body">
+        <div class="body1">
+            <h1>Por onde começar?</h1>
+            <h3>Diga-nos o que precisa</h3>
+            <p>Responda às nossas perguntas para partilhamos as suas necessidades com especialistas na matéria.</p>
+            <h3>Encontre o seu especialista</h3>
+            <p>Compare, analise e escolha a proposta do especialista que mais lhe enche as medidas. Quando o serviço estiver feito, paga diretamente e negócio fechado.</p>
+            <h3>Partilhe como correu</h3>
+            <p>Avalie a experiencia e o resultado final. Assim vai ajudar alguém que não sabe a encontrar um serviço de qualidade.</p>
+        </div>
+    </div>
+    
 
+
+
+
+    <!--Rodapé contactos -->
+
+    <section class="rodape">
+        <div class="rodape container px-5 py-24 mx-auto">
+          <div class="flex flex-col text-center w-full mb-12">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
+          </div>
+          <div class="lg:w-1/2 md:w-2/3 mx-auto">
+            <div class="flex flex-wrap -m-2">
+              <div class="p-2 w-1/2">
+                <div class="relative">
+                  <label for="name" class="campos leading-7 text-sm text-gray-600">Name</label>
+                  <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                </div>
+              </div>
+              <div class="p-2 w-1/2">
+                <div class="relative">
+                  <label for="email" class="campos leading-7 text-sm text-gray-600">Email</label>
+                  <input type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                </div>
+              </div>
+              <div class="p-2 w-full">
+                <div class="relative">
+                  <label for="message" class="campos leading-7 text-sm text-gray-600">Message</label>
+                  <textarea id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                </div>
+              </div>
+              <div class="relative p-2 w-full">
+                <button class="button">Enviar</button>
+              </div>
+              <div class="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+                <a class="text-indigo-500">example@email.com</a>
+              </div>
+            </div>
+        </div>
+      </section>
 
 </body>
 </html>
