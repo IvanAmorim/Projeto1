a8ccd6c72903d50def9fcc8cd5886a97085c0104
IvanAmorim <93208193+IvanAmorim@users.noreply.github.com>
2022-12-26 22:43:37 +0000

    26/12/2022

--- pkproject/resources/views/job.blade.php
+++ pkproject/resources/views/job.blade.php
@@ -4,18 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="/css/job.css" rel="stylesheet"> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons@latest/iconfont/tabler-icons.min.css">
+    
     <title>Document</title>
 </head>
 <body>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/4388aefc94.js" crossorigin="anonymous"></script>
+
     
     <div class="wrap">
-        <div class="card">
-            <div class="container">
-                <h4>Pergunta</h4>
-                <input type="checkbox" name="resposta1" id="resposta1" value="resposta">
-                <label for="resposta1">write were</label><br>
-            </div>
-        </div>
+
+        <?php
+            foreach ($perguntas as $pergunta) {
+                echo('<div class="card">
+                        <div class="container">');
+                echo("<h4>$pergunta->Pergunta</h4>");
+                foreach ($respostas as $resposta) {
+                    if($resposta->ID_pergunta == $pergunta->ID)
+                        echo("<input type='checkbox' id='$resposta->ID' value='$resposta->Resposta'>
+                                <label for='$resposta->ID'>$resposta->Resposta</label><br>");
+                }
+                echo('</div>
+                        </div>');
+            }
+
+        ?>                
+
     </div>
 </body>
 </html>
+  
