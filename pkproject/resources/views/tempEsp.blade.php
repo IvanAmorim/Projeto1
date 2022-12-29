43800ce3bd4f880d53304c6683d58f7d05687d93
Jose <zebaptista26@gmail.com>
2022-12-29 14:48:40 +0000

    teste parte especialista

---
+++ pkproject/resources/views/tempEsp.blade.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="/css/tempEsp.css" rel="stylesheet"> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons@latest/iconfont/tabler-icons.min.css">
+    
+    <title>@yield('titulo')</title>
+</head>
+<body>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/4388aefc94.js" crossorigin="anonymous"></script>
+
+
+    <div class="container">
+        <div id="request_wrapper">           
+            <div class="request_box new">
+                <div class="request_box_lft">
+                    <a href="/request/1374167_31d81bad755e878fc39d014dcfbb0e326a4b1582_2/quote" class="reqLink">
+                    </a>
+                    <div class="row-fluid">
+                        <a href="/request/1374167_31d81bad755e878fc39d014dcfbb0e326a4b1582_2/quote" class="reqLink">
+                            <div class="left">
+                                <h4>
+                                    teste 
+                                    <div class="online_status online_now">
+                                        <div class="online_tooltip">
+                                            <span>
+                                                Online
+                                            </span>
+                                        </div>
+                                    </div>
+                                </h4>
+                                <p class="category_p">
+                                    <span class="cat_tex">
+                                        teste2
+                                    </span>
+                                </p>
+                                <p>
+                                    teste3
+                                </p>
+                                <p>
+                                    <button class="btn btn-primary read_request">
+                                        Ver detalhes
+                                    </button>
+                                </p>
+                            </div>
+                        </a>
+                        <div class="request_box_rgt tar right pr10 mr25">
+                            <a href="/request/1374167_31d81bad755e878fc39d014dcfbb0e326a4b1582_2/quote" class="reqLink">
+                            </a>
+                            <a href="/request/1374167_31d81bad755e878fc39d014dcfbb0e326a4b1582_2/quote" class="reqLink">
+                                há &lt; 1 minuto
+                                <!--Hide RID from available requests tab by using css class-->
+                                <div class="ss hide">
+                                    ID 1374167
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="global_requests_badge_wrapper">
+                    </div>
+                </div>
+            </div>
+
+            <div class="request_box new">
+                <div class="request_box_lft">
+                    <a href="/request/1374167_31d81bad755e878fc39d014dcfbb0e326a4b1582_2/quote" class="reqLink">
+                    </a>
+                    <div class="row-fluid">
+                        <a href="/request/1374167_31d81bad755e878fc39d014dcfbb0e326a4b1582_2/quote" class="reqLink">
+                            <div class="left">
+                                <h4>
+                                    teste 
+                                    <div class="online_status online_now">
+                                        <div class="online_tooltip">
+                                            <span>
+                                                Online
+                                            </span>
+                                        </div>
+                                    </div>
+                                </h4>
+                                <p class="category_p">
+                                    <span class="cat_tex">
+                                        teste2
+                                    </span>
+                                </p>
+                                <p>
+                                    teste3
+                                </p>
+                                <p>
+                                    <button class="btn btn-primary read_request">
+                                        Ver detalhes
+                                    </button>
+                                </p>
+                            </div>
+                        </a>
+                        <div class="request_box_rgt tar right pr10 mr25">
+                            <a href="/request/1374167_31d81bad755e878fc39d014dcfbb0e326a4b1582_2/quote" class="reqLink">
+                            </a>
+                            <a href="/request/1374167_31d81bad755e878fc39d014dcfbb0e326a4b1582_2/quote" class="reqLink">
+                                há &lt; 1 minuto
+                                <!--Hide RID from available requests tab by using css class-->
+                                <div class="ss hide">
+                                    ID 1374167
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="global_requests_badge_wrapper">
+                    </div>
+                </div>
+            </div>
+
+            <div class="request_box new">
+                <div class="request_box_lft">
+                    <a href="/request/1374167_31d81bad755e878fc39d014dcfbb0e326a4b1582_2/quote" class="reqLink">
+                    </a>
+                    <div class="row-fluid">
+                        <a href="/request/1374167_31d81bad755e878fc39d014dcfbb0e326a4b1582_2/quote" class="reqLink">
+                            <div class="left">
+                                <h4>
+                                    teste 
+                                    <div class="online_status online_now">
+                                        <div class="online_tooltip">
+                                            <span>
+                                                Online
+                                            </span>
+                                        </div>
+                                    </div>
+                                </h4>
+                                <p class="category_p">
+                                    <span class="cat_tex">
+                                        teste2
+                                    </span>
+                                </p>
+                                <p>
+                                    teste3
+                                </p>
+                                <p>
+                                    <button class="btn btn-primary read_request">
+                                        Ver detalhes
+                                    </button>
+                                </p>
+                            </div>
+                        </a>
+                        <div class="request_box_rgt tar right pr10 mr25">
+                            <a href="/request/1374167_31d81bad755e878fc39d014dcfbb0e326a4b1582_2/quote" class="reqLink">
+                            </a>
+                            <a href="/request/1374167_31d81bad755e878fc39d014dcfbb0e326a4b1582_2/quote" class="reqLink">
+                                há &lt; 1 minuto
+                                <!--Hide RID from available requests tab by using css class-->
+                                <div class="ss hide">
+                                    ID 1374167
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="global_requests_badge_wrapper">
+                    </div>
+                </div>
+            </div>
+
+            <div class="request_box new">
+                <div class="request_box_lft">
+                    <a href="/request/1374167_31d81bad755e878fc39d014dcfbb0e326a4b1582_2/quote" class="reqLink">
+                    </a>
+                    <div class="row-fluid">
+                        <a href="/request/1374167_31d81bad755e878fc39d014dcfbb0e326a4b1582_2/quote" class="reqLink">
+                            <div class="left">
+                                <h4>
+                                    teste 
+                                    <div class="online_status online_now">
+                                        <div class="online_tooltip">
+                                            <span>
+                                                Online
+                                            </span>
+                                        </div>
+                                    </div>
+                                </h4>
+                                <p class="category_p">
+                                    <span class="cat_tex">
+                                        teste2
+                                    </span>
+                                </p>
+                                <p>
+                                    teste3
+                                </p>
+                                <p>
+                                    <button class="btn btn-primary read_request">
+                                        Ver detalhes
+                                    </button>
+                                </p>
+                            </div>
+                        </a>
+                        <div class="request_box_rgt tar right pr10 mr25">
+                            <a href="/request/1374167_31d81bad755e878fc39d014dcfbb0e326a4b1582_2/quote" class="reqLink">
+                            </a>
+                            <a href="/request/1374167_31d81bad755e878fc39d014dcfbb0e326a4b1582_2/quote" class="reqLink">
+                                há &lt; 1 minuto
+                                <!--Hide RID from available requests tab by using css class-->
+                                <div class="ss hide">
+                                    ID 1374167
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="global_requests_badge_wrapper">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
